db1586cbf2b8efb936bbb88eab40de1ab446585d
sylee <seungyeoni.dev@gmail.com>
2025-06-10 17:32:46 +0900

    [Dev] 공사중 - 툴팁메뉴 추가

--- src/components/organisms/TooltipMenu.tsx
+++ src/components/organisms/TooltipMenu.tsx
@@ -6,19 +6,23 @@ const TooltipMenu = () => {
   return (
     <div className="menu-wrap">
       <button
-        className="toggle-btn"
-        aria-label="메뉴 열기"
+        className={`toggle-btn${isActive ? " active" : ""}`}
+        aria-label={`메뉴 ${isActive ? "닫기" : "열기"}`}
         onClick={() => setIsActive(!isActive)}
-      ></button>
+      >
+        <span className="line line1"></span>
+        <span className="line line2"></span>
+        <span className="line line3"></span>
+      </button>
       {isActive && (
         <div className="menu-area">
           <ul>
             <li>
-              <h2 className="menu-title">다크모드 설정</h2>
+              <h2 className="menu-title">• 테마 설정</h2>
               <DarkModeButton />
             </li>
             <li>
-              <h2 className="menu-title">공유하기</h2>
+              <h2 className="menu-title">• 공유하기</h2>
               <button>공유하기 버튼</button>
             </li>
           </ul>
